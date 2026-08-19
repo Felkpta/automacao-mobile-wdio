d47bf1538e60195fd4d83aba6ff6a2a58381bbef
Felkpta <cruzeiro933@gmail.com>
2026-08-18 21:23:39 -0300

    Corrige seletores e navegacao do login (acaba pelo amor de Deus)

--- test/pageobjects/login.page.js
+++ test/pageobjects/login.page.js
@@ -1,10 +1,16 @@
 class LoginPage {
+  get tabBarLogin() { return $('~Login'); }
+  get loginScreen() { return $('~Login-screen'); }
+  get loginContainerButton() { return $('~button-login-container'); }
   get inputEmail() { return $('~input-email'); }
   get inputPassword() { return $('~input-password'); }
   get btnLogin() { return $('~button-LOGIN'); }
   get successMessage() { return $('android=new UiSelector().textContains("Success")'); }
 
   async login(email, password) {
+    await this.tabBarLogin.click();
+    await this.loginScreen.waitForDisplayed();
+    await this.loginContainerButton.click();
     await this.inputEmail.setValue(email);
     await this.inputPassword.setValue(password);
     await this.btnLogin.click();
