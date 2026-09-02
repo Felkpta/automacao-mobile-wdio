1b764c1af1ff7cb030c9eb2771c6987f066c8854
Felkpta <cruzeiro933@gmail.com>
2026-09-02 09:54:34 -0300

    Refactor LoginPage class for improved clarity
    Refactor LoginPage class by removing unused elements and simplifying login method.

--- test/pageobjects/login.page.js
+++ test/pageobjects/login.page.js
@@ -1,20 +1,12 @@
 class LoginPage {
-  get tabBarLogin() { return $('~Login'); }
-  get loginScreen() { return $('~Login-screen'); }
-  get loginContainerButton() { return $('~button-login-container'); }
-  get inputEmail() { return $('~input-email'); }
-  get inputPassword() { return $('~input-password'); }
-  get btnLogin() { return $('~button-LOGIN'); }
-  get successMessage() { return $('android=new UiSelector().textContains("Success")'); }
+    get inputEmail() { return $('~email'); }
+    get inputPassword() { return $('~password'); }
+    get btnLogin() { return $('~btnLogin'); }
 
-  async login(email, password) {
-    await this.tabBarLogin.click();
-    await this.loginScreen.waitForDisplayed();
-    await this.loginContainerButton.click();
-    await this.inputEmail.setValue(email);
-    await this.inputPassword.setValue(password);
-    await this.btnLogin.click();
-  }
+    async login(email, password) {
+        await this.inputEmail.setValue(email);
+        await this.inputPassword.setValue(password);
+        await this.btnLogin.click();
+    }
 }
-
 module.exports = new LoginPage();
