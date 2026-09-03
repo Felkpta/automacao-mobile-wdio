18a743592f6fc73401b8c6b337fbf80082fefc80
Felkpta <cruzeiro933@gmail.com>
2026-09-03 13:36:14 +0000

    fix: adiciona download do APK na pipeline para o BrowserStack

--- wdio.bs.conf.js
+++ wdio.bs.conf.js
@@ -4,12 +4,17 @@ export const config = {
     ...baseConfig,
     user: process.env.BROWSERSTACK_USERNAME,
     key: process.env.BROWSERSTACK_ACCESS_KEY,
-    services: ['browserstack'],
+    services: [
+        ['browserstack', {
+            app: './app/native-demo-app.apk',
+            testObservability: true
+        }]
+    ],
     capabilities: [{
         platformName: 'android',
         'appium:platformVersion': '12.0',
         'appium:deviceName': 'Google Pixel 6',
-        'appium:app': 'bs://sample.app', // ou o ID/app carregado no BrowserStack
-        'appium:automationName': 'UiAutomator2'
+        'appium:automationName': 'UiAutomator2',
+        'appium:app': './app/native-demo-app.apk'
     }]
 };
