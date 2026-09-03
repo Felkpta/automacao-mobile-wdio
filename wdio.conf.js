050928065b3b15efe78c63094b148907b7339c9d
Felkpta <cruzeiro933@gmail.com>
2026-09-03 14:14:44 +0000

    ci: adiciona upload de APK via API do BrowserStack

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,19 +1,44 @@
-import path from 'path';
-
 export const config = {
-    runner: 'local',
-    port: 4723,
-    specs: ['./test/specs/**/*.js'],
+    user: process.env.BROWSERSTACK_USERNAME,
+    key: process.env.BROWSERSTACK_ACCESS_KEY,
+    
+    hostname: 'hub.browserstack.com',
+    
+    specs: [
+        './test/specs/**/*.js'
+    ],
+    exclude: [],
+
     maxInstances: 1,
+
     capabilities: [{
         platformName: 'Android',
+        'appium:platformVersion': '12.0',
+        'appium:deviceName': 'Google Pixel 6',
         'appium:automationName': 'UiAutomator2',
-        'appium:deviceName': 'Android Emulator',
-        'appium:app': path.join(process.cwd(), 'app/native-demo-app.apk'),
-        'appium:autoGrantPermissions': true,
+        'appium:app': process.env.BS_APP_URL || './app/native-demo-app.apk',
+        'bstack:options': {
+            projectName: 'Automacao Mobile WDIO',
+            buildName: 'browserstack-build-ci',
+            sessionName: 'Execution Test',
+            debug: true,
+            networkLogs: true
+        }
     }],
+
     logLevel: 'info',
+    bail: 0,
+    waitforTimeout: 10000,
+    connectionRetryTimeout: 120000,
+    connectionRetryCount: 3,
+    
+    services: ['browserstack'],
+    
     framework: 'mocha',
     reporters: ['spec'],
-    mochaOpts: { ui: 'bdd', timeout: 120000 },
+    
+    mochaOpts: {
+        ui: 'bdd',
+        timeout: 60000
+    }
 };
