5de87aa2d92b2b34a61c14ed44499d2ac66b82e6
Felkpta <cruzeiro933@gmail.com>
2026-09-03 13:49:00 +0000

    refactor: migra require/module.exports para ES Modules nos testes

--- test/pageobjects/login.page.js
+++ test/pageobjects/login.page.js
@@ -1,20 +1,20 @@
 class LoginPage {
-  get tabBarLogin() { return $('~Login'); }
-  get loginScreen() { return $('~Login-screen'); }
-  get loginContainerButton() { return $('~button-login-container'); }
-  get inputEmail() { return $('~input-email'); }
-  get inputPassword() { return $('~input-password'); }
-  get btnLogin() { return $('~button-LOGIN'); }
-  get successMessage() { return $('android=new UiSelector().textContains("Success")'); }
+    get tabButtonLogin() { return $('~login'); }
+    get loginScreen() { return $('~login-screen'); }
+    get loginContainerButton() { return $('~button-login-container'); }
+    get inputEmail() { return $('~input-email'); }
+    get inputPassword() { return $('~input-password'); }
+    get btnLogin() { return $('~button-LOGIN'); }
+    get successMessage() { return $('android=new UiSelector().textContains("Success")'); }
 
-  async login(email, password) {
-    await this.tabBarLogin.click();
-    await this.loginScreen.waitForDisplayed();
-    await this.loginContainerButton.click();
-    await this.inputEmail.setValue(email);
-    await this.inputPassword.setValue(password);
-    await this.btnLogin.click();
-  }
+    async login(email, password) {
+        await this.tabButtonLogin.click();
+        await this.loginScreen.waitForDisplayed();
+        await this.loginContainerButton.click();
+        await this.inputEmail.setValue(email);
+        await this.inputPassword.setValue(password);
+        await this.btnLogin.click();
+    }
 }
 
-module.exports = new LoginPage();
+export default new LoginPage();
--- test/specs/login.spec.js
+++ test/specs/login.spec.js
@@ -1,8 +1,8 @@
-const LoginPage = require('../pageobjects/login.page');
+import LoginPage from '../pageobjects/login.page.js';
 
 describe('Fluxo de Login', () => {
-  it('deve logar com sucesso usando credenciais válidas', async () => {
-    await LoginPage.login('teste@email.com', 'senha123');
-    await expect(LoginPage.successMessage).toBeDisplayed();
-  });
+    it('deve logar com sucesso usando credenciais válidas', async () => {
+        await LoginPage.login('teste@email.com', 'senha123');
+        await expect(LoginPage.successMessage).toBeDisplayed();
+    });
 });
